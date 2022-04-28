00fc4601bb42cacc63db71df46f298553717602f
GitHub Action <action@github.com>
2022-04-28 14:44:29 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/nucle/intellijquasarlivetemplates/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nucle.intellijquasarlivetemplates.services
+
+import com.github.nucle.intellijquasarlivetemplates.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
