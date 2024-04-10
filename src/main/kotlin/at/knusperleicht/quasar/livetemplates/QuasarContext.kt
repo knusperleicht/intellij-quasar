53a7049dc59c39807e810f43c2f09767a61f6a59
Christian Behon <christian.behon@knusperleicht.at>
2024-04-10 21:01:10 +0200

    #13 feat(2024): Upgrade template to the newest version and adopt template changes

--- src/main/kotlin/at/knusperleicht/quasar/livetemplates/QuasarContext.kt
+++ src/main/kotlin/at/knusperleicht/quasar/livetemplates/QuasarContext.kt
@@ -3,7 +3,7 @@ package at.knusperleicht.quasar.livetemplates
 import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
 
-class QuasarContext : TemplateContextType("VUE", "Vue") {
+class QuasarContext : TemplateContextType("Vue") {
     override fun isInContext(templateActionContext: TemplateActionContext): Boolean {
         return templateActionContext.file.name.endsWith(".vue")
     }
